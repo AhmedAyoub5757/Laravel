58a6b3ad222d5101307a824be52b735f020bfdcc
AhmedAyoub5757 <kkahmed5757@gmail.com>
2026-08-18 10:23:29 +0500

    routing basics

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+// Create a route for /about that returns the text "This is the About page"
 Route::get('/about', function (){
     return view('about');
 });
+// Create a route for /greet/{name} that returns "Hello, {name}!" using the captured parameter.
+Route::get('/greet/{name}', function ($name){
+    return "Hello, $name!";
+});
+// Create a route for /profile/{username?} where the parameter is optional, defaulting to "Guest" if not provided.
+Route::get('/profile/{username?}', function($username = "Guest"){
+    return "hello, $username!";
+});
+
+Route::get('/test', function (){
+    return view('test');
+})->name('test');
